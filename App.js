b68c449107352e6393c4e0ac7aad86d5377c4da9
Filip Koza <38352025+Fil1989@users.noreply.github.com>
2022-02-10 04:49:24 +0100

    form-finished

--- App.js
+++ App.js
@@ -1,43 +1,78 @@
 
-import React from 'react'
-import { StyleSheet, Text, View, ImageBackground, TextInput, Button } from 'react-native';
-import image from './assets/back-image.jpg'
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import image from './assets/back-image.jpg';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+const loadApplication = async ()=>{
+  await Font.loadAsync({'Festive-Regular': require('./assets/fonts/Festive-Regular.ttf')});
+}
 export default function App() {
-  const [text, onChangeText] = React.useState(null);
-  const [password, onChangePassword] = React.useState(null);
-
+const initialState={
+  email:'',
+  password:'',
+}
+const [state, setState] = React.useState(initialState);
+  const [key, setKey] = React.useState(false);
+  const [isReady, setIsReady] = React.useState(false);
+const hideKeyboard = ()=>{
+  setKey(false);
+  Keyboard.dismiss();
+}
+const sendData = ()=>{
+  setKey(false);
+  Keyboard.dismiss();
+  console.log(state);
+  setState(initialState);
+}
+if(!isReady){
+  return <AppLoading startAsync={loadApplication}
+  onFinish={() => setIsReady(true)} 
+  onError={console.warn}
+  />
+}
   return (
+    <TouchableWithoutFeedback onPress={hideKeyboard}>
     <View style={styles.container}>
-      <ImageBackground source={image} style={styles.image}>
+      <ImageBackground source={image} style={{...styles.image, justifyContent: key?'flex-end':'center'}} resizeMode='cover'
+>
       <Text style={styles.moto}>Never give up!</Text>
-      <View style={styles.form}>
+      <View style={{...styles.form, marginBottom: key? 10 : 0,
+}}>
         <View style={styles.field}>
           <Text style={styles.inputTitle}>Email</Text>
         <TextInput
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={(value)=>setState((previousState)=>({...previousState, email:value}))}
+        value={state.email}
         textAlign={'center'}
         placeholder="useless placeholder"
-        // secureTextEntry={true}
+        onFocus={()=>{setKey(true)}}
       />
         </View>
         <View style={styles.field}>
           <Text style={styles.inputTitle}>Password</Text>
         <TextInput
         style={styles.input}
-        onChangeText={onChangePassword}
-        value={password}
+        onChangeText={(value)=>setState((previousState)=>({...previousState, password:value}))}
+        value={state.password}
         textAlign={'center'}
         placeholder="useless placeholder"
         secureTextEntry={true}
+        onFocus={()=>{setKey(true)}}
+
       />
         </View>
-     <Button title='Sign In' />
+     <TouchableOpacity style={styles.btn} activeOpacity={0.6} onPress={sendData}>
+       <Text>Sign in</Text> 
+     </TouchableOpacity>
+
       </View>
      
       </ImageBackground>
     </View>
+    </TouchableWithoutFeedback>
+
   );
 }
 
@@ -45,18 +80,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    fontSize: 15,
   },
   moto: {
     color: 'tomato', 
-    fontSize: 29, 
-    fontWeight: 'bold',
-    marginBottom:20
+    fontSize: 59, 
+    marginBottom:20,
+    fontFamily: 'Festive-Regular',
+    textAlign: 'center'
     },
     image: {
       flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    resizeMode: 'cover'
     },
     form: {
       marginHorizontal: 40,
@@ -66,7 +100,7 @@ const styles = StyleSheet.create({
     },
     input: { 
       height: 40,
-      width: 300,
+      width: '100%',
 marginTop:5,
       borderWidth: 2,
       padding: 10,
@@ -74,5 +108,15 @@ marginTop:5,
       borderColor: 'gray',
     },
     inputTitle: {
+    },
+    btn:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor:`#00ffff`,
+      height:40,
+      textAlign: 'center',
+      borderRadius: 5,
+marginHorizontal:20,
+marginTop: 20,
     }
 });
